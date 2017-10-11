24e767a6f3461e5dfddd286583f039f50f88e9b9
Matt <6519811+mattgogerly@users.noreply.github.com>
2017-10-11 15:05:33 +0100

    Misc changes

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,4 @@
 
-public class GridState {
+public class Node {
 
 }
--- src/Solve.java
+++ src/Solve.java
@@ -2,5 +2,11 @@
 public class Solve {
 	public static void main(String[] args) {
 		Grid grid = new Grid();
+		Grid grid2 = new Grid();
+		
+		grid.printGrid();
+		grid2.printGrid();
+		
+		System.out.println(grid.compareGrid(grid2));
 	}
 }
