82fd5c7291aaa8e701ad75060495ede8bdfad255
Matt <6519811+mattgogerly@users.noreply.github.com>
2017-10-11 17:10:50 +0100

    Revamped to test Breadth First Search

--- src/Solve.java
+++ src/Solve.java
@@ -1,12 +1,42 @@
+import java.util.ArrayList;
 
 public class Solve {
+	
 	public static void main(String[] args) {
-		Grid grid = new Grid();
-		Grid grid2 = new Grid();
+		Grid startState = new Grid();
 		
-		grid.printGrid();
-		grid2.printGrid();
+		char[][] goalGrid = createGoalGrid(3, 3);
+		Grid goalState = new Grid(goalGrid, 3, 3);
 		
-		System.out.println(grid.compareGrid(grid2));
+		BreadthFirstSearch bfs = new BreadthFirstSearch();
+		bfs.searchForSolution(startState, goalState);
 	}
+	
+	public static char[][] createGoalGrid(int agentX, int agentY) {
+		char[][] goalGrid = new char[4][4];
+		
+		for (int i = 0; i < 4; i++) {
+			goalGrid[0][i] = 'X';
+		}
+		
+		goalGrid[1][0] = 'X';
+		goalGrid[1][1] = 'A';
+		goalGrid[1][2] = 'B';
+		goalGrid[1][3] = 'C';
+		
+		goalGrid[2][0] = 'X';
+		goalGrid[2][1] = 'X';
+		goalGrid[2][2] = 'X';
+		goalGrid[2][3] = 'X';
+		
+		goalGrid[3][0] = 'X';
+		goalGrid[3][1] = 'X';
+		goalGrid[3][2] = 'X';
+		goalGrid[3][3] = 'X';
+		
+		goalGrid[agentX][agentY] = 'T';
+		
+		return goalGrid;
+	}
+	
 }
