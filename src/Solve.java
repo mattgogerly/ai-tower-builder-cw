6f8e23e6da6acb2fa082d7fcb2f4d9db64be5079
Matt <mg11g16@soton.ac.uk>
2017-11-21 18:42:12 +0000

    Commented out BFS (needs mega RAM and eclipse flags not working)

--- src/Solve.java
+++ src/Solve.java
@@ -5,17 +5,17 @@ public class Solve {
 		
 		Grid goalState = createGoalGrid("XXXXXABCXXXXXXXT", 4, 4);
 		
-		BreadthFirstSearch bfs = new BreadthFirstSearch();
-		bfs.searchForSolution(startState, goalState, false);
+		// BreadthFirstSearch bfs = new BreadthFirstSearch();
+		// bfs.searchForSolution(startState, goalState, false);
 		
-		// DepthFirstSearch dfs = new DepthFirstSearch();
-		// dfs.searchForSolution(startState, goalState, false);
+		DepthFirstSearch dfs = new DepthFirstSearch();
+		dfs.searchForSolution(startState, goalState, false);
 		
-		// IterativeDeepeningSearch ids = new IterativeDeepeningSearch();
-		// ids.searchForSolution(startState, goalState, false);
+		IterativeDeepeningSearch ids = new IterativeDeepeningSearch();
+		ids.searchForSolution(startState, goalState, false);
 		
-		// AStarSearch ass = new AStarSearch();
-		// ass.searchForSolution(startState, goalState, false);
+		AStarSearch ass = new AStarSearch();
+		ass.searchForSolution(startState, goalState, false);
 	}
 	
 	public static Grid createGoalGrid(String input, int width, int height) {
